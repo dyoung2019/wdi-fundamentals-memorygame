e38a4fbc4273b467a8aefaa92da8f5020886f48b
David Young <tgsstdio@gmail.com>
2019-09-25 12:55:03 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,45 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: "images/queens-of-hearts.png",
+	},
+	{
+		color: 'queen',
+		suit: 'diamonds',
+		cardImage: "images/queens-of-diamonds.png",
+	},
+	{
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: "images/king-of-hearts.png",
+	},
+	{
+		color: 'king',
+		suit: 'diamonds',
+		cardImage: "images/king-of-diamonds.png",
+	}	
+];
 
 var cardsInPlay = [];
 
 function checkForMatch() {
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
-			console.log("You found a match!");
+			alert("You found a match!");
 		}
 		else 
 		{
-			console.log("Sorry, try again.");
+			alert("Sorry, try again.");
 		}
 	}
 }
 
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 
 	checkForMatch();
 }
